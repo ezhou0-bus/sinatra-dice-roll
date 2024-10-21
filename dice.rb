3f675a7016e6034fff984680e594f7ee552cbd7d
ezhou0-bus <elainedai999@gmail.com>
2024-10-21 21:07:52 +0000

    6

--- dice.rb
+++ dice.rb
@@ -10,16 +10,16 @@ get("/") do
   erb(:elephant)
 end
 
-        get("/dice/2/6") do
-          first_die = rand(1..6)
-          second_die = rand(1..6)
-          sum = first_die + second_die
-          
-          outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-          
-          "<h1>2d6</h1>
-          <p>#{outcome}</p>"
-        end
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+  
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
+  erb(:two_six)
+
+end     
 
         get("/dice/2/10") do
           first_die = rand(1..10)
