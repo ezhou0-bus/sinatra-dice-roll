cd40dca902ace54317a063998d43551c62f7e247
ezhou0-bus <elainedai999@gmail.com>
2024-10-21 21:19:22 +0000

    8

--- dice.rb
+++ dice.rb
@@ -26,19 +26,20 @@ end
           second_die = rand(1..10)
           sum = first_die + second_die
           
-          outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-          
-          "<h1>2d10</h1>
-          <p>#{outcome}</p>"
+          @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+          erb(:two_ten)
         end
+        
+        
         get("/dice/1/20") do
           die = rand(1..20)
           
-          outcome = "You rolled a #{die}."
+          @outcome = "You rolled a #{die}."
           
-          "<h1>1d20</h1>
-          <p>#{outcome}</p>"
+          erb(:one_twenty)
         end
+
+
         get("/dice/5/4") do
           first_die = rand(1..4)
           second_die = rand(1..4)
@@ -46,8 +47,7 @@ end
           fourth_die = rand(1..4)
           sum = first_die + second_die + third_die + fourth_die
           
-          outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-          
-          "<h1>5d4</h1>
-          <p>#{outcome}</p>"
+          @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+          erb(:five_four)
+         
         end
